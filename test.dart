eabac13d6d63b28fb3b2538874b3a447af32b8eb
99TheDark <99TheDark@gmail.com>
2023-06-05 12:19:50 -0700

    Continuing

--- test.dart
+++ test.dart
@@ -1,8 +1,18 @@
+import 'dart:math';
+
+dynamic getRandom(List<dynamic> list) {
+  var rand = Random();
+  return list[rand.nextInt(list.length)];
+}
+
 void main() {
   print('Hello world!');
   var x = {
     'a': 5,
     'b': [3, 1, 4]
   };
-  print(x['a']); // why no dot formatting :( -> basically JSON
+  // why no dot formatting :( -> basically JSON
+  // really annoying to traverse though, like typescript but harder
+  int val = getRandom(x['b'] as List);
+  print(val);
 }
